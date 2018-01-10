b87a345ac73f0aaaf9446c742be81fcef54e21d8
Hoàng Phúc <hoangphucd14cn05@gmail.com>
2018-01-10 21:54:03 +0700

    L?n d?u dua lên github

---
+++ app/src/main/java/hoangphucsvmc/com/store_on_github/MainActivity.kt
@@ -0,0 +1,12 @@
+package hoangphucsvmc.com.store_on_github
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
